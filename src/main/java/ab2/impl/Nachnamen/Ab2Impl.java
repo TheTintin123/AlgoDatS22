f6559176a9633afe95e253a4f302e46037b0fc9b
christofplie <97967480+christofplie@users.noreply.github.com>
2022-06-04 13:56:33 +0200

    Very Quickselect

--- src/main/java/ab2/impl/Nachnamen/Ab2Impl.java
+++ src/main/java/ab2/impl/Nachnamen/Ab2Impl.java
@@ -74,10 +74,75 @@ public class Ab2Impl extends AbstractHashMap implements Ab2 {
 		return this;
 	}
 
-	@Override
-	public int quickselect(int[] data, int i) {
-		// TODO Auto-generated method stub
-		return 0;
+@Override
+	public  int quickselect(int[] data, int i) {
+		return (data[check(data, 0, data.length - 1, i)]);
+	}
+	static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	static void insertSort(int[] a, int beg, int end) {
+		for (int i = beg; i < end; ++i) {
+			int key = a[i];
+			int j = i - 1;
+			while (j >= beg && a[j] > key) {
+				a[j + 1] = a[j];
+				j = j - 1;
+			}
+			a[j + 1] = key;
+		}
+	}
+
+
+	//Lomuto partition
+	static int partition(int[] a, int l, int r) {
+		int pivot = a[r];
+		int i = (l - 1);
+		for (int j = l; j < r; j++) {
+			if (a[j] <= pivot) {
+				i++;
+				swap(a, i, j);
+			}
+		}
+		swap(a, i + 1, r);
+		return i + 1;
+	}
+
+	static int check(int[] a, int l, int r, int elem) {
+		// in-place median of medians algorithm
+		// a[] - array
+		// l - element "from"
+		// r - element "to"
+		// elem - kth element we're looking for
+		if (r - l <= 5) {
+			insertSort(a, l, r + 1);
+			return l + elem - 1;
+		} else {
+			int count = l;
+			for (int i = l; i < r; i = i + 5) {
+				if (i + 5 < r) {
+					insertSort(a, i, i + 5);
+					swap(a, count, i + 2);
+					// swapping the found medians with elements
+					// at the beginning of the considered array (from "l")
+					// instead of creating a new array to store medians
+				} else {
+					insertSort(a, i, r);
+					swap(a, count, (i + r) / 2);
+				}
+				count++;
+			}
+			int medianaMedian = check(a, l, count - 1, (count - l + 1) / 2);
+			swap(a, medianaMedian, r);
+			int pivot = partition(a, l, r);
+			int elem2 = pivot - l + 1;
+			if (elem == elem2) return pivot;
+			else if (elem < elem2) return check(a, l, pivot - 1, elem);
+			else return check(a, pivot + 1, r, elem - elem2);
+		}
 	}
 
 	@Override
--- src/test/java/ab2/Ab2Tests.java
+++ src/test/java/ab2/Ab2Tests.java
@@ -20,136 +20,153 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Ab2Tests {
 
-    private Random rand = new Random(System.currentTimeMillis());
+	private Random rand = new Random(System.currentTimeMillis());
 
-    private static Ab2 ab2Impl = new Ab2Impl();
+	private static Ab2 ab2Impl = new Ab2Impl();
 
-    private static final int MOM_TEST_COUNT = 10;
-    private static final int MOM_TEST_MINSIZE = 1;
-    private static final int MOM_TEST_MAXSIZE = (int) Math.pow(2, 10);
+	private static final int MOM_TEST_COUNT = 10;
+	private static final int MOM_TEST_MINSIZE = 1;
+	private static final int MOM_TEST_MAXSIZE = (int) Math.pow(2, 10);
 
-    private static final int HASH_TEST_COUNT = 10;
-    private static final int HASH_TEST_MINSIZE = 1;
-    private static final int HASH_TEST_MAXSIZE = (int) Math.pow(2, 10);
-    private static final double HASH_MAX_LOADFACTOR = 0.5;
+	private static final int HASH_TEST_COUNT = 10;
+	private static final int HASH_TEST_MINSIZE = 1;
+	private static final int HASH_TEST_MAXSIZE = (int) Math.pow(2, 10);
+	private static final double HASH_MAX_LOADFACTOR = 0.5;
 
-    @Test
-    public void testHashLinear() {
-	//Test die tatsächliche Größe der Hashtabelle
-	AbstractHashMap hm = ab2Impl.newHashMapLinear(14);
-	assertEquals(14, hm.capacity());
 
-	checkFullMap(hm);
+	@Test
+	public void testHashLinear() {
+		//Test die tatsächliche Größe der Hashtabelle
+		AbstractHashMap hm = ab2Impl.newHashMapLinear(14);
+		assertEquals(14, hm.capacity());
 
-	for (int size = HASH_TEST_MINSIZE; size <= HASH_TEST_MAXSIZE; size *= 2) {
+		checkFullMap(hm);
 
-	    int maxElements = (int) Math.max(size * HASH_MAX_LOADFACTOR, 1);
-	    for (int i = 0; i < HASH_TEST_COUNT; i++) {
-		AbstractHashMap hashMap = ab2Impl.newHashMapLinear(size);
+		for (int size = HASH_TEST_MINSIZE; size <= HASH_TEST_MAXSIZE; size *= 2) {
 
-		testAbstractHashMap(hashMap, maxElements);
-	    }
+			int maxElements = (int) Math.max(size * HASH_MAX_LOADFACTOR, 1);
+			for (int i = 0; i < HASH_TEST_COUNT; i++) {
+				AbstractHashMap hashMap = ab2Impl.newHashMapLinear(size);
+
+				testAbstractHashMap(hashMap, maxElements);
+			}
+		}
 	}
-    }
 
-    @Test
-    public void testHashQuadratic() {
-	//Test die tatsächliche Größe der Hashtabelle
-	AbstractHashMap hm = ab2Impl.newHashMapQuadratic(14);
-	assertEquals(19, hm.capacity());
+	@Test
+	public void testHashQuadratic() {
+		//Test die tatsächliche Größe der Hashtabelle
+		AbstractHashMap hm = ab2Impl.newHashMapQuadratic(14);
+		assertEquals(19, hm.capacity());
 
-	checkFullMap(hm);
+		checkFullMap(hm);
 
-	for (int size = HASH_TEST_MINSIZE; size <= HASH_TEST_MAXSIZE; size *= 2) {
+		for (int size = HASH_TEST_MINSIZE; size <= HASH_TEST_MAXSIZE; size *= 2) {
 
-	    int maxElements = (int) Math.max(size * HASH_MAX_LOADFACTOR, 1);
-	    for (int i = 0; i < HASH_TEST_COUNT; i++) {
-		AbstractHashMap hashMap = ab2Impl.newHashMapQuadratic(size);
+			int maxElements = (int) Math.max(size * HASH_MAX_LOADFACTOR, 1);
+			for (int i = 0; i < HASH_TEST_COUNT; i++) {
+				AbstractHashMap hashMap = ab2Impl.newHashMapQuadratic(size);
 
-		testAbstractHashMap(hashMap, maxElements);
-	    }
+				testAbstractHashMap(hashMap, maxElements);
+			}
+		}
 	}
-    }
 
-    @Test
-    public void testHashDouble() {
-	//Test die tatsächliche Größe der Hashtabelle
-	AbstractHashMap hm = ab2Impl.newHashMapDouble(14);
-	assertEquals(17, hm.capacity());
+	@Test
+	public void testHashDouble() {
+		//Test die tatsächliche Größe der Hashtabelle
+		AbstractHashMap hm = ab2Impl.newHashMapDouble(14);
+		assertEquals(17, hm.capacity());
 
-	checkFullMap(hm);
+		checkFullMap(hm);
 
-	for (int size = HASH_TEST_MINSIZE; size <= HASH_TEST_MAXSIZE; size *= 2) {
+		for (int size = HASH_TEST_MINSIZE; size <= HASH_TEST_MAXSIZE; size *= 2) {
 
-	    int maxElements = (int) Math.max(size * HASH_MAX_LOADFACTOR, 1);
-	    for (int i = 0; i < HASH_TEST_COUNT; i++) {
-		AbstractHashMap hashMap = ab2Impl.newHashMapDouble(size);
+			int maxElements = (int) Math.max(size * HASH_MAX_LOADFACTOR, 1);
+			for (int i = 0; i < HASH_TEST_COUNT; i++) {
+				AbstractHashMap hashMap = ab2Impl.newHashMapDouble(size);
 
-		testAbstractHashMap(hashMap, maxElements);
-	    }
+				testAbstractHashMap(hashMap, maxElements);
+			}
+		}
 	}
-    }
 
-    @Test
-    public void testMediansOfMedians() {
-	for (int size = MOM_TEST_MINSIZE; size <= MOM_TEST_MAXSIZE; size *= 2) {
-	    int[] data = new int[size];
+	@Test
+	public void testMediansOfMedians() {
+		for (int size = MOM_TEST_MINSIZE; size <= MOM_TEST_MAXSIZE; size *= 2) {
+			int[] data = new int[size];
 
-	    for (int i = 0; i < MOM_TEST_COUNT; i++) {
-		fillRandomInts(data, MOM_TEST_MAXSIZE);
+			for (int i = 0; i < MOM_TEST_COUNT; i++) {
+				fillRandomInts(data, MOM_TEST_MAXSIZE);
 
-		// kopiere das Array, sortiere es und bestimme den Median (das length/2-kleinste Element)
-		int[] data_sorted = Arrays.copyOf(data, data.length);
+				// kopiere das Array, sortiere es und bestimme den Median (das length/2-kleinste Element)
+				int[] data_sorted = Arrays.copyOf(data, data.length);
+				Arrays.sort(data_sorted);
+				int median_expected = data_sorted[data_sorted.length / 2];
+
+				// bestimme den berechneten Wert laut Implementierung
+				int median = ab2Impl.quickselect(data, data.length / 2 + 1);
+
+				assertEquals(median_expected, median);
+			}
+		}
+	}
+
+	@Test
+	public void testQuickSelect(){
+		int[] arr = new int[500000];
+		fillRandomInts(arr,1000);
+
+		int[] data_sorted = Arrays.copyOf(arr, arr.length);
 		Arrays.sort(data_sorted);
-		int median_expected = data_sorted[data_sorted.length / 2];
 
-		// bestimme den berechneten Wert laut Implementierung
-		int median = ab2Impl.quickselect(data, data.length / 2 + 1);
+		int kPosition = rand.nextInt(arr.length);
 
-		assertEquals(median_expected, median);
-	    }
-	}
-    }
+		int k_expected = data_sorted[kPosition];
+		int k = ab2Impl.quickselect(arr, kPosition);
 
-    private void fillRandomInts(int[] data, int maxValue) {
-	for (int i = 0; i < data.length; i++) {
-	    data[i] = rand.nextInt(maxValue + 1);
-	}
-    }
-
-    private void checkFullMap(AbstractHashMap hashMap) {
-	for(int i = 0; i < hashMap.capacity(); i++) {
-	    assertEquals(true, hashMap.put(i, i+""));
+		assertEquals(k_expected, k);
 	}
 
-	assertEquals(false, hashMap.put(-1, "ein Wert zu viel"));
-    }
-
-    private void testAbstractHashMap(AbstractHashMap hashMap, int elementCount) {
-	int capacity = hashMap.capacity();
-
-	// Als Referenz wird eine Map erzeugt, um die Implementierung zu testen
-	Map<Integer, String> hashMapRef = new HashMap<>(capacity);
-
-	// Befülle die HashMaps
-	for (int i = 0; i < elementCount; i++) {
-	    int key = rand.nextInt(capacity);
-	    String value = "Wert " + key;
-
-	    hashMap.put(key, value);
-	    hashMapRef.put(key, value);
+	private void fillRandomInts(int[] data, int maxValue) {
+		for (int i = 0; i < data.length; i++) {
+			data[i] = rand.nextInt(maxValue + 1);
+		}
 	}
 
-	assertEquals(hashMapRef.size(), hashMap.count());
+	private void checkFullMap(AbstractHashMap hashMap) {
+		for (int i = 0; i < hashMap.capacity(); i++) {
+			assertEquals(true, hashMap.put(i, i + ""));
+		}
 
-	// Teste, ob die Werte wiedergefunden werden
-	for (int i = 0; i < 10*elementCount; i++) {
-	    int key = rand.nextInt(capacity);
-
-	    String value = hashMap.get(key);
-	    String valueRef = hashMapRef.get(key);
-
-	    assertEquals(valueRef, value);
+		assertEquals(false, hashMap.put(-1, "ein Wert zu viel"));
 	}
-    }
+
+	private void testAbstractHashMap(AbstractHashMap hashMap, int elementCount) {
+		int capacity = hashMap.capacity();
+
+		// Als Referenz wird eine Map erzeugt, um die Implementierung zu testen
+		Map<Integer, String> hashMapRef = new HashMap<>(capacity);
+
+		// Befülle die HashMaps
+		for (int i = 0; i < elementCount; i++) {
+			int key = rand.nextInt(capacity);
+			String value = "Wert " + key;
+
+			hashMap.put(key, value);
+			hashMapRef.put(key, value);
+		}
+
+		assertEquals(hashMapRef.size(), hashMap.count());
+
+		// Teste, ob die Werte wiedergefunden werden
+		for (int i = 0; i < 10 * elementCount; i++) {
+			int key = rand.nextInt(capacity);
+
+			String value = hashMap.get(key);
+			String valueRef = hashMapRef.get(key);
+
+			assertEquals(valueRef, value);
+		}
+	}
 }
